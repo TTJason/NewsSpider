dd49bc47c2cce6beb2ce0cce34d3daa1b21dafc6
sviber <echocchhoo@gmail.com>
2017-08-21 16:40:23 +0800

    process_news

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 
 host = 'localhost'
 username = 'root'
-password = '123456'
+password = '!@#$%^'
 database_name = 'news_dataset'
 
-LTP_DATA_DIR = '/home/tt/Desktop/NLP_DATA/ltp_data'
-LYRIC_BASE_PATH = '/home/tt/Desktop/NLP_APP/Generate_Lyric/one_word_one_sentence'
+LTP_DATA_DIR = '/Users/sivber/Desktop/NLP_DATA/ltp_data'
+LYRIC_BASE_PATH = '/Users/sivber/Desktop/NLP_APP/Generate_Lyric/one_word_one_sentence'
 lyric_source_path = LYRIC_BASE_PATH+'/lyric.char.source'
 lyric_target_path = LYRIC_BASE_PATH+'/lyric.char.target'
+
+TENCENT_BASE_PATH = '/Users/sivber/Desktop/NLP_DATA/news'
+TITLE_PATH = TENCENT_BASE_PATH+'/title'
---
+++ process_data/process_news.py
@@ -0,0 +1,26 @@
+import pymysql
+import config
+import re
+
+
+def write_data_from_db_to_file(file_name):
+    source_file = open(file_name, 'w')
+    db = pymysql.connect(config.host, config.username, config.password, config.database_name, charset="utf8")
+    cursor = db.cursor()
+    sql = "select title from tencent where title <> '' and cname='中国足球'"
+    print(sql)
+    # 执行SQL语句
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    for l, in result:
+        title = re.sub(r'GIF-', '', l).strip()
+        if '：' in title:
+            title = title.split('：')[1]
+        if ':' in title:
+            title = title.split(':')[1]
+        source_file.write(title)
+        source_file.write('\n')
+    db.close()
+
+    source_file.close()
+write_data_from_db_to_file(config.TITLE_PATH)
